c7c6d4c895a05b373dfaa05bd952df9545aa08aa
Igor Muntyan <igor.muntyan@gmail.com>
2020-12-28 14:14:47 -0500

    putting everything together

--- web/__init__.py
+++ web/__init__.py
@@ -3,6 +3,8 @@ import numpy as np
 from urllib.request import urlopen
 import cv2
 import jsonpickle
+from .image_utils import imageprepare
+from tensorflow import keras
 
 app = Flask(__name__)
 
@@ -24,10 +26,22 @@ def image():
     print(data)
     with urlopen(data) as response:
         dt = response.read()
+
+
+        print(dt)
         # convert string of image data to uint8
+        # nparr = np.fromstring(dt, np.uint8)
+
+        digit = 0
+        tva, new_image = imageprepare(dt)
+        xnp = np.array(tva, dtype="float32").reshape((1,28, 28,1))
+        model = keras.models.load_model('mnist_fitted_model')
+        digit = model.predict(xnp)
+        print(digit)
+
         nparr = np.fromstring(dt, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+        response = {'message': 'image received. size={}x{}, digit={}'.format(img.shape[1], img.shape[0], digit)}
         response_pickled = jsonpickle.encode(response)
         return Response(response=response_pickled, status=200, mimetype="application/json")
 
---
+++ web/image_utils.py
@@ -0,0 +1,57 @@
+from PIL import Image, ImageFilter, ImageOps
+import io
+
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').getchannel('A')
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+    else:
+        return im
+
+
+def imageprepare(image_data):
+    """
+    This function returns the pixel values.
+    The input is a png file location.
+    """
+    im = Image.open(io.BytesIO(image_data))
+    im = remove_transparency(im)
+    im = im.resize((100,100))
+    width = float(im.size[0])
+    height = float(im.size[1])
+    new_image = Image.new('L', (28, 28), 255)  # creates white canvas of 28x28 pixels
+
+    if width > height:  # check which dimension is bigger
+        # Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
+        if nheight == 0:  # rare case but minimum is 1 pixel
+            nheight = 1
+            # resize and sharpen
+        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
+        new_image.paste(img, (4, wtop))  # paste resized image on white canvas
+    else:
+        # Height is bigger. Heigth becomes 20 pixels.
+        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
+        if nwidth == 0:  # rare case but minimum is 1 pixel
+            nwidth = 1
+            # resize and sharpen
+        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+        new_image.paste(img, (wleft, 4))  # paste resized image on white canvas
+
+    new_image = ImageOps.invert(new_image)
+
+    tv = list(new_image.getdata())  # get pixel values
+
+    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
+    return tva, new_image
